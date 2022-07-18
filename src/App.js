ea6f3b1424104197a8716b30c284ae6267948cd3
Zaryl-Zh <zaryl.abdrazakova@gmail.com>
2022-07-18 14:52:07 +0600

    set state with categories, types, sizes

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
 import Sort from './components/Sort';
 import './scss/app.scss';
-
-
+import pizzas from './assets/pizzas.json';
 
 function App() {
   return (
-    <div className ="wrapper">
-    <Header/>
-      <div className ="content">
-        <div className ="container">
-          <div className ="content__top">
-            <Categories/>
-            <Sort/>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
           </div>
-          <h2 className ="content__title">All pizza</h2>
-          <div className ="content__items">
-          <PizzaBlock title = 'Pepperoni' price={340}/>
-          <PizzaBlock title = 'Margaritta' price={440}/>
-       
+          <h2 className="content__title">All pizza</h2>
+          <div className="content__items">
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj} />
+            ))}
           </div>
         </div>
       </div>
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = ['All', 'Meat', 'Vegetarian', 'Grilled', 'Spicy', 'Closed'];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
   return (
     <div className="categories">
-    <ul>
-      <li className="active">All</li>
-      <li>Meat</li>
-      <li>Vegetarian</li>
-      <li>Grilled</li>
-      <li>Spicy</li>
-      <li>Closed</li>
-    </ul>
-  </div>
-  )
-}
+      <ul>
+        {categories.map((pizza, index) => (
+          <li
+            key={index}
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? 'active' : ''}>
+            {pizza}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Categories
+export default Categories;
